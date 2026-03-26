6ada826d34ebb355918f11d6d3df8f978d02f409
gabriel.almeida <gabriel.almeida@dcx.ufpb.br>
2026-03-26 12:11:43 -0300

    finalizado

---
+++ src/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Aviao meuAviao = new Aviao("Boeing 747");
+
+        meuAviao.ligarTurbinas();
+        meuAviao.voar();
+    }
+}
